f5df3f3122bfb28f0a66a0303bfdbf578476d983
Raikuzu <robman455@gmail.com>
2024-07-04 07:27:55 -0400

    added CSS for index and started logic as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 console.log('hat')
+const column = document.querySelector('column');
+
+function submitForm(){
+    var email= document.getElementById('input1').value;
+    var username= document.getElementById('input2').value;
+    var comments = document.getElementById('comments').value;
+
+    //console.log('Email:' email);
+    console.log('Username', username);
+    console.log('Comments', comments);
+
+
+}
